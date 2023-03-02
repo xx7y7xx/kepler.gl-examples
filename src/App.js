1b56ab2e451795c6055c16e05926dbdd5d51033a
devin.chenyang <devin.chenyang@grabtaxi.com>
2023-03-02 22:20:15 +0800

    load new data to map

--- src/App.js
+++ src/App.js
@@ -1,60 +1,32 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import KeplerGl from 'kepler.gl';
 import { addDataToMap } from 'kepler.gl/actions';
-import './App.css';
 
-const mapData = {
-  datasets: {
-    info: {
-      label: 'Sample Taxi Trips in New York City',
-      id: 'test_trip_data'
-    },
-    data: {
-      fields: [
-        {name: 'tpep_pickup_datetime', format: 'YYYY-M-D H:m:s', type: 'timestamp'},
-        {name: 'pickup_longitude', format: '', type: 'real'},
-        {name: 'pickup_latitude', format: '', type: 'real'}
-      ],
-      rows: [
-        ['2015-01-15 19:05:39 +00:00', 103.801770, 1.273607],
-        ['2015-01-15 19:05:39 +00:00', 103.822990, 1.275041],
-      ]
-    },
-  },
-  options: {
-    centerMap: true,
-    readOnly: false,
-    keepExistingConfig: false
-  },
-  info: {
-    title: 'Taro and Blue',
-    description: 'This is my map'
-  },
-  config: {
-    visState: {
-      filters: [
-        {
-          id: 'me',
-          dataId: 'test_trip_data',
-          name: 'tpep_pickup_datetime',
-          type: 'timeRange',
-          enlarged: true
-        }
-      ]
-    }
-  },
-};
+import { initMapToLoad } from './constants';
+import { getNewMapToLoad } from './utils';
+
+import './App.css';
 
 function App() {
   const dispatch = useDispatch();
+  const fooState = useSelector(state => state.keplerGl.foo);
   
   useEffect(() => {
-    dispatch(addDataToMap(mapData));
-  }, [dispatch])
+    dispatch(addDataToMap(
+      initMapToLoad
+    ));
+  }, [dispatch]);
+
+  const handleClick = () => {
+    dispatch(addDataToMap(
+      getNewMapToLoad(fooState)
+    ));
+  }
 
   return (
     <div className="App">
+      <button onClick={handleClick}>Load data</button>
       <KeplerGl
         id="foo"
         mapboxApiAccessToken="pk.eyJ1IjoidWJlcmRhdGEiLCJhIjoidGllX1gxUSJ9.gElUooDF7u51guCQREmAhg"
---
+++ src/constants.js
@@ -0,0 +1,51 @@
+export const pointLayerDatasetId = 'point-layer-dataset-id';
+export const polygonLayerDatasetId = 'polygon-layer-dataset-id';
+
+export const pointLayerFieldsDefinition = [
+  { name: 'longitude', format: '', type: 'real' },
+  { name: 'latitude', format: '', type: 'real' }
+];
+
+export const polygonLayerFieldsDefinition = [
+  { name: 'name', format: '', type: 'string' },
+  { name: 'polygon', format: '', type: 'geojson' }
+];
+
+export const initMapToLoad = {
+  info: {
+    title: 'Point and Polygon',
+    description: 'Will display points and polygons on the map'
+  },
+  datasets: [{
+    info: { label: 'Point data', id: pointLayerDatasetId },
+    data: {
+      fields: pointLayerFieldsDefinition,
+      rows: [
+        [103.801770, 1.273607],
+        [103.822990, 1.275041],
+      ]
+    },
+  }, {
+    info: { label: 'Polygon data', id: polygonLayerDatasetId },
+    data: {
+      fields: polygonLayerFieldsDefinition,
+      rows: [
+        ['VivoCity', '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"coordinates":[[[103.8198,1.2655],[103.8199,1.2631],[103.8234,1.2631],[103.8235,1.2655],[103.8198,1.2655]]],"type":"Polygon"}}]}'],
+      ]
+    },
+  }],
+  options: {
+    centerMap: true,
+    readOnly: false,
+    keepExistingConfig: false
+  },
+  config: {
+    visState: {
+      filters: []
+    }
+  },
+};
+
+export const newPoints = [
+  [103.82482, 1.26473],
+];
