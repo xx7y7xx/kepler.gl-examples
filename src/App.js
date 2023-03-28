a754f7c1428bbebf2c2f3108c92133f8a5e6b0f6
devin.chenyang <devin.chenyang@grabtaxi.com>
2023-03-28 15:22:13 +0800

    load new data to map step by step

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import KeplerGl from 'kepler.gl';
-import { addDataToMap } from 'kepler.gl/actions';
+import { addDataToMap, updateMap } from 'kepler.gl/actions';
+import KeplerGlSchema from 'kepler.gl/schemas';
 
+import { POINT_DATASET_ID, newPoints, POLYGON_DATASET_ID, newPolygons } from './constants';
 import { initMapToLoad } from './constants';
-import { getNewMapToLoad } from './utils';
 
 import './App.css';
 
+const keplerglMapId = 'foo';
+
 function App() {
   const dispatch = useDispatch();
-  const fooState = useSelector(state => state.keplerGl.foo);
+  const fooState = useSelector(state => state.keplerGl[keplerglMapId]);
   
   useEffect(() => {
     dispatch(addDataToMap(
@@ -18,21 +21,88 @@ function App() {
     ));
   }, [dispatch]);
 
-  const handleClick = () => {
-    dispatch(addDataToMap(
-      getNewMapToLoad(fooState)
-    ));
+  const handleMoveToCityCenter = () => {
+    dispatch(
+      updateMap({ latitude: 1.305886, longitude: 103.82108, zoom: 10 }),
+    );
+  }
+
+  const handleLoadPointData = () => {
+    // save current map data and config
+    const { datasets, config } = KeplerGlSchema.save(fooState);
+    console.debug('Before loading point data', datasets, config)
+
+    const newDatasets = datasets.map(dataset => {
+      if (dataset.data.id !== POINT_DATASET_ID) {
+        return dataset;
+      }
+      return ({
+        version: dataset.version,
+        data: {
+          ...dataset.data,
+          allData: [
+            ...dataset.data.allData,
+            ...newPoints,
+          ],
+        }
+      });
+    });
+
+    // load config with new datasets
+    const mapToLoad = KeplerGlSchema.load(newDatasets, config);
+
+    dispatch(addDataToMap({
+      ...mapToLoad,
+      options: { centerMap: false }
+    }));
+  }
+
+  const handleLoadPolygonData = () => {
+    // save current map data and config
+    const { datasets, config } = KeplerGlSchema.save(fooState);
+    console.debug('Before loading polygon data', datasets, config)
+
+    const newDatasets = datasets.map(dataset => {
+      if (dataset.data.id !== POLYGON_DATASET_ID) {
+        return dataset;
+      }
+      return ({
+        version: dataset.version,
+        data: {
+          ...dataset.data,
+          allData: [
+            ...dataset.data.allData,
+            ...newPolygons,
+          ],
+        }
+      });
+    });
+
+    // load config with new datasets
+    const mapToLoad = KeplerGlSchema.load(newDatasets, config);
+
+    dispatch(addDataToMap({
+      ...mapToLoad,
+      options: { centerMap: false }
+    }));
   }
 
   return (
     <div className="App">
-      <button onClick={handleClick}>Load data</button>
-      <KeplerGl
-        id="foo"
-        mapboxApiAccessToken="pk.eyJ1IjoidWJlcmRhdGEiLCJhIjoidGllX1gxUSJ9.gElUooDF7u51guCQREmAhg"
-        width={1200}
-        height={800}
-      />
+      <button onClick={handleMoveToCityCenter}>Move to city center</button>
+      <button onClick={handleLoadPointData}>Load point data</button>
+      <button onClick={handleLoadPolygonData}>Load polygon data</button>
+      <div className='keplergl-wrapper'>
+        <KeplerGl
+          id={keplerglMapId}
+          mapboxApiAccessToken="pk.eyJ1IjoidWJlcmRhdGEiLCJhIjoidGllX1gxUSJ9.gElUooDF7u51guCQREmAhg"
+          width={1200}
+          height={800}
+        />
+      </div>
+      <div>{(fooState?.visState?.layers || []).map(l => (
+        <div key={l.id}>{l.id} | {l.config.dataId} | {l.type}</div>
+      ))}</div>
     </div>
   );
 }
--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,7 @@
-export const pointLayerDatasetId = 'point-layer-dataset-id';
-export const polygonLayerDatasetId = 'polygon-layer-dataset-id';
-
-export const pointLayerFieldsDefinition = [
-  { name: 'longitude', format: '', type: 'real' },
-  { name: 'latitude', format: '', type: 'real' }
-];
-
-export const polygonLayerFieldsDefinition = [
-  { name: 'name', format: '', type: 'string' },
-  { name: 'polygon', format: '', type: 'geojson' }
-];
+export const POINT_DATASET_ID = 'point-layer-dataset-id';
+export const POINT_LAYER_ID = 'point-layer-id';
+export const POLYGON_DATASET_ID = 'polygon-layer-dataset-id';
+export const POLYGON_LAYER_ID = 'polygon-layer';
 
 export const initMapToLoad = {
   info: {
@@ -17,35 +9,72 @@ export const initMapToLoad = {
     description: 'Will display points and polygons on the map'
   },
   datasets: [{
-    info: { label: 'Point data', id: pointLayerDatasetId },
+    info: { label: 'Point data', id: POINT_DATASET_ID },
     data: {
-      fields: pointLayerFieldsDefinition,
+      fields: [
+        { name: 'shop_longitude', format: '', type: 'real' },
+        { name: 'shop_latitude', format: '', type: 'real' }
+      ],
       rows: [
-        [103.801770, 1.273607],
-        [103.822990, 1.275041],
+        // [103.801770, 1.273607],
+        // [103.822990, 1.275041],
       ]
     },
   }, {
-    info: { label: 'Polygon data', id: polygonLayerDatasetId },
+    info: { label: 'Polygon data', id: POLYGON_DATASET_ID },
     data: {
-      fields: polygonLayerFieldsDefinition,
+      fields: [
+        { name: 'name', format: '', type: 'string' },
+        { name: 'mall_polygon', format: '', type: 'geojson' }
+      ],
       rows: [
-        ['VivoCity', '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"coordinates":[[[103.8198,1.2655],[103.8199,1.2631],[103.8234,1.2631],[103.8235,1.2655],[103.8198,1.2655]]],"type":"Polygon"}}]}'],
+        // ['VivoCity', '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"coordinates":[[[103.8198,1.2655],[103.8199,1.2631],[103.8234,1.2631],[103.8235,1.2655],[103.8198,1.2655]]],"type":"Polygon"}}]}'],
       ]
     },
   }],
   options: {
-    centerMap: true,
+    centerMap: false, // default is true
     readOnly: false,
     keepExistingConfig: false
   },
   config: {
     visState: {
-      filters: []
+      filters: [],
+      layers:[{
+        id: POINT_LAYER_ID,
+        type: 'point',
+        config: {
+          dataId: POINT_DATASET_ID,
+          label: 'Point Layer',
+          color: [255, 0, 0], // #ff0000
+          columns: {
+            lat: 'shop_latitude',
+            lng: 'shop_longitude',
+            altitude: null,
+          },
+          isVisible: true
+        },
+      },{
+        id: POLYGON_LAYER_ID,
+        type: 'geojson',
+        config: {
+          dataId: POLYGON_DATASET_ID,
+          label: 'Polygon Layer',
+          color: [67, 143, 69], // #438F45
+          columns: {
+            geojson: 'mall_polygon',
+          },
+          isVisible: true
+        },
+      }]
     }
-  },
+  }
 };
 
 export const newPoints = [
   [103.82482, 1.26473],
 ];
+
+export const newPolygons = [
+  ['VivoCity', '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"coordinates":[[[103.8198,1.2655],[103.8199,1.2631],[103.8234,1.2631],[103.8235,1.2655],[103.8198,1.2655]]],"type":"Polygon"}}]}'],
+]
